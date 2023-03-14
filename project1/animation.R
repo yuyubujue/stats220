93fe83f629822530c25a0cc61127e8db2a2a02cc
yuyubujue <1936967743@qq.com>
2023-03-14 09:29:46 +0800

    project1 upload

---
+++ project1/animation.R
@@ -0,0 +1,12 @@
+library(magick) #import magick
+
+f1 <- image_read("frame-1.jpg") #read frames
+f2 <- image_read("frame-2.jpg")
+f3 <- image_read("frame-3.jpg")
+f4 <- image_read("frame-4.jpg")
+f5 <- image_read("frame-5.jpg")
+f6 <- image_read("frame-6.jpg")
+gif <- image_animate(c(f1,f2,f3,f4,f5,f6)) #use image_animate to create a 6-frame gif
+
+print(gif)
+image_write(gif, "my_animation.gif") #save my gif
---
+++ project1/meme.R
@@ -0,0 +1,16 @@
+library(magick) #import magick
+
+meme<- image_read("frame-4.jpg")   #use image_read()
+
+new_meme <- image_blank(10,164,"yellow")  #use image_blank()
+
+
+
+new_meme <- image_append(c(meme,new_meme)) %>%  # use pipe,image_append() and c()
+  image_rotate(5) %>%
+  image_annotate("You",color = "white",location ='+45+35',size = 30) %>%
+  image_annotate("Life",color = "white",location ='+120+30',size = 30)
+
+print(new_meme)
+image_info(new_meme)
+image_write(new_meme,"new_meme.png")

a58fcf443947b882c78e8d81751ce0935a144e89
yuyubujue <1936967743@qq.com>
2023-03-14 13:57:42 +0800

    project1 upload

--- project1/meme.R
+++ project1/meme.R
@@ -1,6 +1,6 @@
 library(magick) #import magick
 
-meme<- image_read("frame-4.jpg")   #use image_read()
+meme<- image_read("images/frame-4.jpg")   #use image_read()
 
 new_meme <- image_blank(10,164,"yellow")  #use image_blank()
 
@@ -8,8 +8,9 @@ new_meme <- image_blank(10,164,"yellow")  #use image_blank()
 
 new_meme <- image_append(c(meme,new_meme)) %>%  # use pipe,image_append() and c()
   image_rotate(5) %>%
-  image_annotate("You",color = "white",location ='+45+35',size = 30) %>%
-  image_annotate("Life",color = "white",location ='+120+30',size = 30)
+  image_blur(2, 1) %>%
+  image_annotate("You",color = "white",location ='+45+35',size = 30,boxcolor = "pink") %>%
+  image_annotate("Life",color = "white",location ='+120+30',size = 30,boxcolor = "pink")
 
 print(new_meme)
 image_info(new_meme)
